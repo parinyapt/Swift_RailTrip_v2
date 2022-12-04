2e66df2b5492a22f60adf70ba5de3f05a85b029c
parinyapt <parinyapt99@gmail.com>
2022-12-04 23:18:10 +0700

    add email validate to login page

--- RailTrip/utils.backendConnect.swift
+++ RailTrip/utils.backendConnect.swift
@@ -6,9 +6,49 @@
 //
 
 import Foundation
+import Alamofire
 
 class BackendConnect {
-    func demoprint() {
-        print("print OK")
+    let BASE_URL = "https://railtrip-api.prinpt.com/api/v1"
+    let API_VERSION = "v1"
+    
+    struct RequestOTPResponse: Codable {
+        let refID: String
+        let status: String
+        
+        enum CodingKeys: String, CodingKey {
+            case status
+            case refID = "ref_id"
+        }
+    }
+    
+    func RequestOTP(email:String, completion: @escaping (DefaultAPIResponse<RequestOTPResponse>?, Bool) -> Void) {
+        typealias reponseStruct = DefaultAPIResponse<RequestOTPResponse>
+        let url = "\(BASE_URL)/auth/request-otp"
+        
+        let headers: HTTPHeaders = [
+            "Accept": "application/json",
+        ]
+        
+        let parameters: [String: String] = [
+            "email": email
+        ]
+        
+        AF.request(
+            url,
+            method: .post,
+            parameters: parameters,
+            encoder: JSONParameterEncoder.default,
+            headers: headers
+        ).responseDecodable(of: reponseStruct.self) { response in
+            switch(response.result){
+            case .success:
+                completion(response.value,false)
+                break
+            case .failure:
+                completion(response.value,true)
+                break
+            }
+        }
     }
 }
